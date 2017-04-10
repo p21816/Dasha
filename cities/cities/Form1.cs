549033671b38098651a4a8f1f3edfeab7e27898d
DaryaBattalova <dariabattalova@gmail.com>
2017-04-10 14:08:46 +0300

    made a new project about musical instruments (2 ComboBoxes) downloaded from XML file

---
+++ cities/cities/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace cities
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument instruments = new XmlDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            instruments.Load("../../instruments.xml");
+            var brandNodes = instruments.SelectNodes("instruments/type/name");
+            foreach (XmlNode x in brandNodes)
+            {
+                comboBox2.Items.Add(x.InnerText);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string xpath = "instruments/type[name='" + (string)comboBox2.SelectedItem + "']/models/model/name";
+            var modelNodes = instruments.SelectNodes(xpath);
+            comboBox1.Items.Clear();
+            foreach(XmlNode x in modelNodes)
+            {
+                comboBox1.Items.Add(x.InnerText);
+            }
+        }
+    }
+}
