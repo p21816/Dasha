cfd02cbc1cdb459e2301a855f0ef49e75f227a5c
DaryaBattalova <dariabattalova@gmail.com>
2017-05-04 14:39:46 +0300

    added mouse_up event

--- CraphicEditor/CraphicEditor/Form1.cs
+++ CraphicEditor/CraphicEditor/Form1.cs
@@ -16,7 +16,7 @@ namespace CraphicEditor
         Pen p = new Pen(Brushes.Aqua);
         Rectangle chosenRectangle = new Rectangle();
         Point mouseClick;
-        int index=-1;
+        int index = -1;
 
         List<Rectangle> rect = new List<Rectangle>();
 
@@ -48,21 +48,30 @@ namespace CraphicEditor
                 num++;
             }
             return -1;
-            
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             p.Width = 1;
+            //Point point1 = new Point(50, 50);
+            //Point point2 = new Point(100, 25);
+            //Point point3 = new Point(200, 50);
+            //Point[] curvePoints =
+            // {
+            //     point1,
+            //     point2,
+            //     point3
+            // };
 
             foreach(Rectangle r in rect)
             {
-                e.Graphics.DrawRectangle(p,
-                 r);
+                e.Graphics.DrawRectangle(p, r);
+
+                //e.Graphics.DrawPolygon(p, curvePoints);
             }
 
             //e.Graphics.DrawEllipse(p,
-            //     new Rectangle(CircleCoordinates[0], CircleCoordinates[1], CircleCoordinates[2], CircleCoordinates[3]));
+            //     new Rectangle(10 , 10, 50, 50));
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
@@ -72,6 +81,14 @@ namespace CraphicEditor
                 rect[index] = new Rectangle(e.Location.X - mouseClick.X + chosenRectangle.X, e.Location.Y - mouseClick.Y + chosenRectangle.Y, chosenRectangle.Width, chosenRectangle.Height);
                 this.Invalidate();
                 this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(index != -1)
+            {
+                index = -1;
             }
         }
     }
