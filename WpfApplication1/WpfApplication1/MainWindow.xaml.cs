bd1f8eec32c64ec24289aaa0b0a6d9130dd79572
DaryaBattalova <dariabattalova@gmail.com>
2017-05-15 17:28:49 +0300

    app drawing smile and x-mas tree

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -27,10 +27,67 @@ namespace WpfApplication1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            tree1.Visibility = Visibility.Hidden;
+            tree2.Visibility = Visibility.Hidden;
+            tree3.Visibility = Visibility.Hidden;
+            tree4.Visibility = Visibility.Hidden;
+            tree5.Visibility = Visibility.Hidden;
+            tree6.Visibility = Visibility.Hidden;
+            tree7.Visibility = Visibility.Hidden;
+            tree8.Visibility = Visibility.Hidden;
+            tree9.Visibility = Visibility.Hidden;
+            tree10.Visibility = Visibility.Hidden;
+            tree11.Visibility = Visibility.Hidden;
+            tree12.Visibility = Visibility.Hidden;
+            tree13.Visibility = Visibility.Hidden;
+            tree14.Visibility = Visibility.Hidden;
+
+
             Eye1.Visibility = Visibility.Visible;
-            Eye1.Content = "X";
             Eye2.Visibility = Visibility.Visible;
-            Eye2.Content = "X";
+            mouth1.Visibility = Visibility.Visible;
+            mouth2.Visibility = Visibility.Visible;
+            mouth3.Visibility = Visibility.Visible;
+            mouth4.Visibility = Visibility.Visible;
+            mouth5.Visibility = Visibility.Visible;
+            mouth6.Visibility = Visibility.Visible;
+            label1.Content = "smile!";
+
+      
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Eye1.Visibility = Visibility.Hidden;
+            Eye2.Visibility = Visibility.Hidden;
+            mouth1.Visibility = Visibility.Hidden;
+            mouth2.Visibility = Visibility.Hidden;
+            mouth3.Visibility = Visibility.Hidden;
+            mouth4.Visibility = Visibility.Hidden;
+            mouth5.Visibility = Visibility.Hidden;
+            mouth6.Visibility = Visibility.Hidden;
+
+
+            tree1.Visibility = Visibility.Visible;
+            tree2.Visibility = Visibility.Visible;
+            tree3.Visibility = Visibility.Visible;
+            tree4.Visibility = Visibility.Visible;
+            tree5.Visibility = Visibility.Visible;
+            tree6.Visibility = Visibility.Visible;
+            tree7.Visibility = Visibility.Visible;
+            tree8.Visibility = Visibility.Visible;
+            tree9.Visibility = Visibility.Visible;
+            tree10.Visibility = Visibility.Visible;
+            tree11.Visibility = Visibility.Visible;
+            tree12.Visibility = Visibility.Visible;
+            tree13.Visibility = Visibility.Visible;
+            tree14.Visibility = Visibility.Visible;
+            label1.Content = "Merry Christmas!!!";
         }
 
 
