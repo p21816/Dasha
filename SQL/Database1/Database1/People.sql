9acb94be1d8e0e1c3ec27c4a78311783b1c9dc85
DaryaBattalova <dariabattalova@gmail.com>
2017-06-06 11:55:18 +0300

    pepole and phones database

---
+++ SQL/Database1/Database1/People.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Firstname] NVARCHAR(MAX) NOT NULL, 
+    [Lastname] NVARCHAR(MAX) NOT NULL, 
+    [DateOfBirth] DATE NULL, 
+    [PersonalNumber] CHAR(16) NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_People_Number] ON [dbo].[People] ([PersonalNumber])
+
+GO
+CREATE INDEX [FullName] ON [dbo].[People] ([Firstname] , [Lastname])
---
+++ SQL/Database1/Database1/Phones.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Phones]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [people_id] INT NOT NULL, 
+    [phone] VARCHAR(MAX) NOT NULL, 
+    CONSTRAINT [FK_Phones_To_People] FOREIGN KEY ([people_id]) REFERENCES [People]([Id])
+	ON DELETE NO ACTION
+)
+
+GO
+
+CREATE INDEX [IX_Phones_Column] ON [dbo].[Phones] ([people_id])
