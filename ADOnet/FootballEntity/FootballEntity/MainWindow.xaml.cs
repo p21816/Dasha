c2860784baa8224d00480f5d744221abcaa7ccd9
DaryaBattalova <dariabattalova@gmail.com>
2017-08-21 11:55:13 +0300

    entityFrameworkExample

---
+++ ADOnet/FootballEntity/FootballEntity/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace FootballEntity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FootballEntities FE = new FootballEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FE.CompetitionWinners.Load();
+            FE.Competitions.Load();
+            dg.ItemsSource = FE.CompetitionWinners.Local;
+            dgComp.ItemsSource = FE.Competitions.Local;
+            TeamsComboBox.Items.Add("hello");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FootballEntity.Competition fec = new FootballEntity.Competition() { name = addCompetitionTextBox.Text , id = Convert.ToInt32(addCompetitionIdTextBox.Text)};
+            FE.Competitions.Add(fec);
+            FE.SaveChanges();
+            FE.Competitions.Load();
+        }
+    }
+}
