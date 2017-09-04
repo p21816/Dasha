c0a479d5ee2ef444da78a67df42103cc291282ee
DaryaBattalova <dariabattalova@gmail.com>
2017-09-04 11:54:17 +0300

    good project with processes

---
+++ System Programming/CountersMaster/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CountersMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal static ObservableCollection<ProcessInfo> pc = new ObservableCollection<ProcessInfo>();
+        internal static Dispatcher dispatcher;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg.ItemsSource = pc;
+            dispatcher = Dispatcher;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pc.Add(new ProcessInfo());
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dg.SelectedItem == null)
+                {
+                    return;
+                }
+                (dg.SelectedItem as ProcessInfo).Priority++;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // INGORE
+            }
+        }
+
+        private void KillButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (dg.SelectedItem == null)
+            {
+                return;
+            }
+            var pi = dg.SelectedItem as ProcessInfo;
+            pi.Kill();
+            pc.Remove(pi);
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dg.SelectedItem == null)
+                {
+                    return;
+                }
+                (dg.SelectedItem as ProcessInfo).Priority--;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // INGORE
+            }
+        }
+    }
+}
---
+++ System Programming/CountersMaster/ProcessInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountersMaster
+{
+    class ProcessInfo:INotifyPropertyChanged
+    {
+        static readonly ProcessPriorityClass[] priorities = new ProcessPriorityClass[] {
+            ProcessPriorityClass.Idle,
+            ProcessPriorityClass.BelowNormal,
+            ProcessPriorityClass.Normal,
+            ProcessPriorityClass.AboveNormal,
+            ProcessPriorityClass.High,
+            ProcessPriorityClass.RealTime
+        };
+
+        private int priority=2;
+        public int Priority
+        {
+            get
+            {
+                return priority;
+            }
+            set
+            {
+                if ((value < 0) || (value >= priorities.Length))
+                {
+                    throw new ArgumentOutOfRangeException("Ooops");
+                }
+                if (value == priority)
+                {
+                    return;
+                }
+                process.PriorityClass = priorities[value];
+                priority = value;
+                OnPropertyChanged("Priority");
+            }
+        }
+
+        public ProcessInfo()
+        {
+            process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo.FileName = @"..\..\..\Counters\bin\Debug\Counters.exe";
+            process.Exited += process_Exited;
+            process.Start();
+        }
+
+        void process_Exited(object sender, EventArgs e)
+        {
+            MainWindow.pc.Remove(this);
+        }
+
+
+        private Process process;
+        public void Kill()
+        {
+            process.Kill();
+        }
+        public int PID { get { return process.Id; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
