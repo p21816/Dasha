b05dcd901ded7902faa9d6bfb1e2ff302bae30ef
DaryaBattalova <dariabattalova@gmail.com>
2017-09-05 11:51:22 +0300

    Threads

---
+++ System Programming/ThreadsDemoWPF/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ThreadsDemoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        List<string> sl = new List<string>();
+        int lastAddedStringNumber = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer t = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, 200),
+                IsEnabled = true,
+            };
+            t.Tick += t_Tick;
+        }
+
+
+
+
+        #region backgroundFibo
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            for (int i = lastAddedStringNumber + 1; i < sl.Count; i++)
+            {
+                outBox.Items.Add(sl[i]);
+                lastAddedStringNumber = i;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread calculator = new Thread(
+                () =>
+                {
+
+                    Random r = new Random();
+                    DateTime start = DateTime.Now;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int n = r.Next(34, 40);
+                        sl.Add(String.Format("{0} {1}", n, FiboMaker.Fibo(n)));
+                    }
+                    sl.Add(String.Format("time = {0:#.##}", (DateTime.Now - start).TotalSeconds));
+                }
+            );
+            calculator.Start();
+        }
+
+        #endregion
+
+
+        int counter;
+        int counts = 100000000;
+        int chunk = 200000;
+
+        object lockObject = new object();
+
+        void inc(object input)
+        {
+            for (int i = 0; i < counts/chunk; i++)
+            {
+                int ooo=0;
+                for (int j = 0; j < chunk; j++)
+                {
+                    ooo++;
+                }
+//                counter+=ooo;
+                Interlocked.Add(ref counter, ooo);
+                //Interlocked.Increment(ref counter);
+            }
+        }
+        void dec(object input)
+        {
+            int param = (int)input;
+            for (int i = 0; i < counts / chunk; i++)
+            {
+                int ooo = 0;
+                for (int j = 0; j < chunk; j++)
+                {
+                    ooo--;
+                }
+                //counter += ooo;
+                Interlocked.Add(ref counter, ooo);
+                //Interlocked.Decrement(ref counter);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            counter = 0;
+            DateTime start = DateTime.Now;
+            Thread t1 = new Thread(inc);
+            Thread t2 = new Thread(dec);
+            t1.Start(0);
+            t2.Start(1);
+            t1.Join();
+            t2.Join();
+            
+            sl.Add(String.Format("time = {0:#.##} res = {1}", (DateTime.Now - start).TotalSeconds,counter));
+
+        }
+    }
+
+    public class SumTask
+    {
+        public readonly int from, to;
+        public int result { get; private set; }
+        void Calculate() {
+            for (int i = from ; i < to; i++)
+            {
+                
+            }
+        }
+    }
+
+    public static class FiboMaker
+    {
+        // O(2^N)
+        static public int Fibo(int n)
+        {
+            if (n < 2) return 1;
+            return Fibo(n - 1) + Fibo(n - 2);
+        }
+    }
+}
---
+++ System Programming/ThreadsWPF/ThreadsWPF/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            long maxNumber = 1000000000;
+            long res = 0;
+
+            sumTask s = new sumTask(0,25);
+            Thread th = new Thread(s.Calculate);
+
+            sumTask s1 = new sumTask(25, 50);
+            Thread th1 = new Thread(s1.Calculate);
+
+            sumTask s2 = new sumTask(50, 75);
+            Thread th2 = new Thread(s2.Calculate);
+
+            sumTask s3 = new sumTask(75, 100);
+            Thread th3 = new Thread(s3.Calculate);
+
+            th.Start();
+
+            th1.Start();
+
+            th2.Start();
+
+            th3.Start();
+
+            th.Join();
+            th1.Join();
+            th2.Join();
+            th3.Join();
+
+            res = s.result + s1.result + s2.result + s3.result;
+            MessageBox.Show(res.ToString());
+
+
+        }
+    }
+
+    public class sumTask
+    {
+        public sumTask(long f, long t)
+        {
+            from = f;
+            to = t;
+        }
+        public readonly long from, to;
+        public long result { get; private set; }
+        public void Calculate()
+        {
+            for (long i = from; i <= to; i++)
+            {
+                result += i;
+            }
+        }
+    }
+
+}
