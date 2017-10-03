933f50884cb32900fff7e7b9463ef7ac321fb1cb
DaryaBattalova <dariabattalova@gmail.com>
2017-10-03 23:48:41 +0300

    linkedList C#

---
+++ epam training/LinkedList/ConsoleApp2/ILinkedList .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public interface ILinkedList
+    {
+        void AddFirst(IListItem item); //добавить элемент в начало
+        void AddLast(IListItem item); //добавить элемент в конец
+        void Insert(IListItem item, int index); //вставить элемент по индексу или в начало, если список пуст
+        bool IsEmpty(); //проверка пустой ли список
+        IListItem GetFirstItem(); //получить первый "корневой" элемент списка
+        IEnumerable<IListItem> GetAll(); //получить все элементы, кроме корневого
+        void Clear(); //очистить список
+        void Reverse(); //отсортировать список в обратном порядке и перестроить связи
+    }
+}
---
+++ epam training/LinkedList/ConsoleApp2/IListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public interface IListItem
+    {
+        IListItem Prev(); //предыдущий элемент
+        IListItem Next(); //следующий элемент 
+        object Value { get; } //значение, хранимое в элементе
+    }
+}
