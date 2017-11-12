c98a8d8de61910d582cc674c0eed38aab17282dd
DaryaBattalova <dariavattalova@gmail.com>
2017-11-12 16:06:30 +0300

    added delegates to Euclidean Algorithm, implemented IFormattable interface to class Book, added delegate to jagged array bubble sort

---
+++ EPAM .NET Training/NET.W.2017.Battalova.06/JaggedArray/NET.W.2017.Battalova.04/IComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2017.Battalova._04
+{
+
+    /// <summary>
+    /// interface for comparison of two strings of a jagged array
+    /// </summary>
+    public interface IComparer
+    {
+        int CompareTo(int[] lhs, int[] rhs);
+    }
+}
