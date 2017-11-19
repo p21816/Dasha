474aa7b4131fbebbbf35835e8f56fbc75dc50257
DaryaBattalova <dariavattalova@gmail.com>
2017-11-19 16:05:31 +0300

    fixed book library

---
+++ EPAM .NET Training/NET.W.2017.Battalova.05.BookLibrary/BookLibrary/NET.W.2017.Battalova.05.BookLibrary/FinderImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2017.Battalova._05.BookLibrary
+{
+    public  class FinderImplementation: IFinder
+    {
+        public bool Find(Book b)
+        {
+            if (b.ISBNProperty == "William Shakespeare") return true;
+            else return false;
+        }
+    }
+}
